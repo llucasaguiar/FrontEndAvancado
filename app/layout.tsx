eaf3bf11f1ed038880cb2897641baeef1d5bc997
lucas <llucasaguiar16@gmail.com>
2026-08-31 01:35:49 -0300

    Primeiro commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <p>Eu sou uma navbar</p>
+        {children}
+        <p>Eu sou um footer</p>
+      </body>
     </html>
   );
 }
